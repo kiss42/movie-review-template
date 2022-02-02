16fa74c700dcc6c041a1b1b259086ca0344739bd
kiss42 <96037516+kiss42@users.noreply.github.com>
2022-02-01 18:48:18 -0600

    my moview

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Categories from '/Users/steven/Documents/my-app/src/Categories.css';
+import config_json from './review_temp/config_momo.json'
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 className="App-header">{"Movie Template "}</h2>
+        <p className="App-header">☑ Awesome</p>
       </header>
+
+      <div className="categories-container">
+        <Categories props={config_json.categories} />
+      </div>
+
+      <footer className="App-footer">
+        <p className="signature">Steven,Pierre</p>
+      </footer>
     </div>
   );
 }
